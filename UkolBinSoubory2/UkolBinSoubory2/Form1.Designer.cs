a6d620b8a731356fb442ceee48c47e581352c9dd
HynekSvoboda <97514770+HynekSvoboda@users.noreply.github.com>
2023-03-28 16:23:25 +0100

    Add files via upload

---
+++ UkolBinSoubory2/UkolBinSoubory2/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace UkolBinSoubory2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(346, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 58);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Přepsání";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(346, 119);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 68);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Výpis";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(328, 288);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(346, 193);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 57);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "?";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(448, 314);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ UkolBinSoubory2/UkolBinSoubory2/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace UkolBinSoubory2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+            button2.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamReader cteni = new StreamReader("sport.txt", Encoding.UTF8);
+            FileStream tok = new FileStream("sport.dat", FileMode.OpenOrCreate, FileAccess.Write);
+            BinaryWriter zapis = new BinaryWriter(tok);
+            while(!cteni.EndOfStream)
+            {
+                string text = cteni.ReadLine();
+                string[] rozdeleni = text.Split(';');
+                int osc = Convert.ToInt32(rozdeleni[0]);
+                string jmeno = rozdeleni[1];
+                string prijmeni = rozdeleni[2];
+                char pohlavi = Convert.ToChar(rozdeleni[3]);
+                int vyska = Convert.ToInt32(rozdeleni[4]);
+                int hmotnost = Convert.ToInt32(rozdeleni[5]);
+                zapis.Write(osc);
+                zapis.Write(jmeno);
+                zapis.Write(prijmeni);
+                zapis.Write(pohlavi);
+                zapis.Write(vyska);
+                zapis.Write(hmotnost);
+            }
+            cteni.Close();
+            tok.Close();
+            button2.Visible = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            FileStream tok = new FileStream("sport.dat", FileMode.Open, FileAccess.Read);
+            BinaryReader cteni = new BinaryReader(tok);
+            cteni.BaseStream.Position = 0;
+            while(cteni.BaseStream.Position<cteni.BaseStream.Length)
+            {
+                textBox1.Text +=cteni.ReadInt32().ToString() + " " + cteni.ReadString() + " " + cteni.ReadString() + " " + cteni.ReadChar().ToString() + " " + cteni.ReadInt32().ToString() + " " + cteni.ReadInt32().ToString()+"\r\n";
+            }
+            tok.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Na disku je textový soubor sport.txt, obsahující na každém řádku\r\nosc;jméno;příjmení;pohlavi;vyska;hmotnost. Vytvořte datový soubor sport.dat, který bude\r\nobsahovat položky\r\n(int osc;String jmeno;String prijmeni;char pohlavi;int vyska;int hmotnost). Soubor zobrazte\r\nv komponentě TextBox.", "INFO");
+        }
+    }
+}
